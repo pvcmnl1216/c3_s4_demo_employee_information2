c38cd4517e868849d3f963268f2c3d09a9f7262d
Divyansh Bhardwaj <divyansh.bhardwaj@niit.com>
2022-08-26 15:06:16 +0530

    created the object for the Manager class and printed the details

--- src/main/java/com/niit/jdp/employee/ManagerImpl.java
+++ src/main/java/com/niit/jdp/employee/ManagerImpl.java
@@ -5,8 +5,25 @@
  */
 package com.niit.jdp.employee;
 
+import java.time.LocalDate;
+
 public class ManagerImpl {
     public static void main(String[] args) {
-
+        Manager manager = new Manager(
+                "Divyansh Bhardwaj",
+                26,
+                LocalDate.of(2022, 8, 26),
+                "Pune",
+                100000,
+                5,
+                5
+        );
+        System.out.println("Name: " + manager.getName());
+        System.out.println("Age: " + manager.getAge());
+        System.out.println("Date of Birth: " + manager.getDateOfBirth());
+        System.out.println("Address: " + manager.getAddress());
+        System.out.println("Salary: " + manager.getSalary());
+        System.out.println("Team Size: " + manager.getTeamSize());
+        System.out.println("Rating: " + manager.getRating());
     }
 }
